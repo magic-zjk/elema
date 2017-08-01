70f782d7ad294c68ddde9a568b4672f518d7617b
magic-zjk <15726613960@163.com>
2017-08-01 09:01:52 +0800

    6-12

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -30,15 +30,7 @@
     <div v-show="detailShow" class="detail">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
+          <h1 class="name">{{seller.name}}</h1>
         </div>
       </div>
       <div class="detail-close">
@@ -195,10 +187,16 @@
       overflow: auto
       background: rgba(7, 17, 27, 0.8)
       .detail-wrapper
+        width: 100%
         min-height: 100%
         .detail-main
           margin-top: 64px
           padding-bottom: 64px
+        .name
+          line-height: 16px
+          text-align: center
+          font-size: 16px
+          font-weight: 700
       .detail-close
         position: relative
         width: 32px
