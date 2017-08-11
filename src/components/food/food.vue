f6e9896753987c3a4ee18f06cd798cfa23be66bc
zhangjiakun <15726613960@163.com>
2017-08-11 17:34:35 +0800

    8-9中未完成

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,10 +1,55 @@
 <template>
   <transition name="move">
-    <div class="food" v-show="showFlag" @click="hidden">dhfehdfe</div>
+    <div class="food" v-show="showFlag" ref="food">
+      <div class="food-content">
+        <div class="image-header">
+          <img :src="food.image">
+          <div class="back" @click="hidden">
+            <i class="icon-arrow_lift"></i>
+          </div>
+        </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+          </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+            <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count ===0">加入购物车</div>
+          </transition>
+        </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc"
+                        :ratings="food.ratings"></ratingselect>
+        </div>
+      </div>
+    </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+  import cartcontrol from '../cartControl/cartController';
+  import split from '../../common/components/split/split';
+  import ratingselect from '../../common/components/ratingselect/ratingselect';
+
+  //  const POSITIVE = 0;
+  //  const NEGATIVE = 1;
+  const ALL = 2;
+
   export default {
     props: {
       food: {
@@ -13,16 +58,46 @@
     },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
       show() {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = true;
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.food, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
       },
       hidden() {
         this.showFlag = false;
+      },
+      addFirst(event) {
+        if (!event._constructed) { // 如果是浏览器原生的话event._constructed返回folse，则阻止；如果是bscroll拆件点击事件则放行，防止pc端出现两次点击
+          return;
+        }
+        this.$root.eventHub.$emit('cart.add', event.target);
+        this.$root.eventHub.$set(this.food, 'count', 1);
       }
+    },
+    components: {
+      cartcontrol,
+      split,
+      ratingselect
     }
   };
 </script>
@@ -36,9 +111,97 @@
     z-index: 30
     width: 100%
     background: #fff
-    transition: all 0.2s
-    &.move-entry-active, &.move-leave-active
+    transition: all 0.4s ease
+    &.move-enter-avtive, &.move-leave-active
       transform: translate3d(0, 0, 0)
-    &.move-entry, &.move-leave-to
+    &.move-enter, &.move-leave-to
       transform: translate3d(100%, 0, 0)
+    .image-header
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+      .back
+        position: absolute
+        top: 10px
+        left: 0
+        .icon-arrow_lift
+          display: block
+          padding: 10px
+          color: #fff
+    .content
+      position: relative
+      padding 18px
+      .title
+        line-height: 14px
+        magin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        line-height: 20px
+        height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700
+        line-height: 24px
+        .now
+          margin-right: 8px
+          font-size: 14px
+          color: rgb(240, 20, 20)
+        .old
+          text-decoration: line-through
+          color: rgb(147, 153, 159)
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #fff
+        background: rgb(0, 160, 220)
+        &.fade-enter-active, &.fade-leave-active
+          transition: opacity 0.2s
+        &.fade-enter, &.fade-leave-to
+          opacity: 0
+
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+    .rating
+      padding:18px
+      .title
+        line-height: 14px
+        font-size: 14px
+        color: rgb(7, 17, 27)
 </style>
